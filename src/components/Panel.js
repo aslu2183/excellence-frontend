5c55ae8a3f2b6be0a995e0b2ad55857c8cc45b69
Mohamed Aslam <aslamk2183@gmail.com>
2023-04-20 14:21:46 +0400

    drag and drop done

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -6,6 +6,7 @@ import PanelCard from "./PanelCard";
 import Typography from '@mui/material/Typography';
 import TaskModal from "./TaskModal";
 import Axios from "src/helpers/api";
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 
 
@@ -58,10 +59,14 @@ function Panel({data, onAction}){
         })
     }
 
-    const updatePanel = (tasks) => {
+    const updatePanel = (data) => {
+        const panelid = data[0].panelId
+        const oldTasks = panelData?.tasks.filter((item) => item.panelId.toString() != panelid.toString())
+        const newTasks = [...oldTasks,...data]
+        
         setpanelData((prevState) => ({
             ...prevState,
-            tasks : tasks
+            tasks : newTasks
         }))
     }
 
@@ -73,14 +78,70 @@ function Panel({data, onAction}){
             </Box>
         )
     }
+
+    const handleOnDragEnd = (result) => {
+        const { source, destination, draggableId } = result
+        if(source.droppableId == destination.droppableId){
+            const panelid = destination.droppableId.split("-")[1] 
+            const panelTasks = [...panelData?.tasks.filter((item) => item.panelId.toString() == panelid.toString())]
+            const [removed] = panelTasks.splice(source.index,1)
+            panelTasks.splice(destination.index, 0, removed);
+            const newTasks = panelTasks.map((item,index) => {
+                return {
+                    ...item,
+                    position : panelTasks.length - index 
+                }
+            })
+            const otherPanelTasks  = panelData?.tasks.filter((item) => item.panelId.toString() != panelid.toString())
+            const updated_tasks    = [...otherPanelTasks,...newTasks]
+            setpanelData((prevState) => ({
+                ...prevState,
+                tasks : updated_tasks
+            }))
+            Axios().post('/reorder-task',{
+                tasks   : newTasks
+            }).then((res) => {
+                console.log("Moved Task ",res.data)
+            }).catch((err) => {
+                console.log("Error ",err)
+            })
+        }
+        else{
+            const taskid  = draggableId.split("-")[1]
+            const panelid = destination.droppableId.split("-")[1] 
+            const filterTask  = panelData?.tasks.filter((item) => item._id.toString() != taskid.toString())    
+            const panelTasks  = panelData?.tasks.filter((item) => item.panelId.toString() == panelid.toString())
+            const currentTask = panelData?.tasks.find((item) => item._id.toString() == taskid.toString())
+            
+            
+            currentTask['panelId'] = panelid
+            currentTask['position']= panelTasks.length + 1
+            const updated_tasks = [...filterTask, { ...currentTask }]
+            
+            setpanelData((prevState) => ({
+                ...prevState,
+                tasks : updated_tasks
+            }))
+
+            Axios().post('/update-task',{
+                panelId : currentTask['panelId'],
+                position: currentTask['position'],
+                taskId  : taskid
+            }).then((res) => {
+                console.log("Moved Task ",res.data)
+            }).catch((err) => {
+                console.log("Error ",err)
+            })
+        }   
+    }
     
     return (
         <Box sx={{display:'flex',overflow:'auto'}}>
-            
+            <DragDropContext onDragEnd={handleOnDragEnd}>
             {
-                panelData?.panels?.map((res) => {
+                panelData?.panels?.map((res,i) => {
                     const tasks = panelData?.tasks.filter((task) => task.panelId.toString() == res._id.toString())
-                    tasks?.sort((a,b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime())
+                    tasks?.sort((a,b) => b.position - a.position)
 
                     const panelCardData = {
                         ...res,
@@ -89,18 +150,26 @@ function Panel({data, onAction}){
                     }
                  
                     return (
-                        <Box sx={{minWidth:280,mb:2,mr:3}} key={res._id}>
-                            <PanelCard 
-                                data={panelCardData} 
-                                onAction={onAction}
-                                openModal={openModalFn}
-                                updatePanelData={updatePanel}>
-                            </PanelCard>
-                        </Box>   
+                        <Droppable droppableId={`panel-${res._id}`} key={res._id}>
+                            {(provided) => (
+                                <div {...provided.droppableProps} ref={provided.innerRef}>
+                                    <Box sx={{minWidth:280,mb:2,mr:3}}>
+                                        <PanelCard 
+                                            data={panelCardData} 
+                                            onAction={onAction}
+                                            openModal={openModalFn}
+                                            updatePanelData={updatePanel}>
+                                        </PanelCard>
+                                    </Box>
+                                </div>
+                            )}
+                        </Droppable>
+                           
                        
                     )
                 })
             }
+            </DragDropContext>
             <TaskModal 
                 open={openModal} 
                 handleClose={closeModalFn} 
